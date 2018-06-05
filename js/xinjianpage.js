78756ebc2cc2ed954af9d4fcf33d4c3a15a73b8a
13453538103@163.com <20140120lian>
2018-06-05 22:19:52 +0800

    6月5日第二次更新

--- js/xinjianpage.js
+++ js/xinjianpage.js
@@ -1,14 +1,3 @@
 jQuery(document).ready(function(){
-    $.ajax({ 
-        'url':'http://47.95.115.143/upload', 
-        'data':$("#updateUserInfo").serialize(),
-        'success':function(data){ 
-            switch(data.type){ 
-                case 0:alert('账户不存在');break; 
-                case 502:alert('fsdf账户不存在');break;
-                    }, 
-        'type':'post',
-        'dataType':'json',
-        'enctype'='multipart/form-data',
-    });
+    
 });
